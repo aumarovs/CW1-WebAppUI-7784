0c98dc82e22448fbc7dfa6b1a8480809b69906d3
UAzam <umarovvv01@gmail.com>
2021-11-09 01:44:29 +0500

    NuGet packages are installed and modedls Product and Category created

---
+++ CW1-WebAppUI-7784/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW1_WebAppUI_7784.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public Category ProductCategory { get; set; }
+    }
+}
